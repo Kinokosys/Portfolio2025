a1ca73200294f73fd9baeb68021194ae62c9300f
Kinokosys <emmawasnotthere@gmail.com>
2025-04-05 10:25:23 +0200

    Veille finie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { GeneralComponent } from './components/general/general.component';
 import { AventureComponent } from './components/aventure/aventure.component';
 import { SavonAPIComponent } from './components/savon-api/savon-api.component';
 import { SavonAPPComponent } from './components/savon-app/savon-app.component';
+import { VeilleComponent } from './components/veille/veille.component';
 
 const routes: Routes = [
   { path: '', component: GeneralComponent },
   { path: 'aventure', component: AventureComponent },
   { path: 'savon-api', component: SavonAPIComponent },
   { path: 'savon-app', component: SavonAPPComponent },
+  { path: 'veille', component: VeilleComponent },
   { path: '**', redirectTo: '' } // Redirige vers l'accueil si la route est inconnue
 ];
 
--- src/app/components/veille/veille.component.ts
+++ src/app/components/veille/veille.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './veille.component.css'
 })
 export class VeilleComponent {
+  // Un tableau d'objets pour suivre l'état de chaque article
+  articles = [
+    { id: 1, visible: false },
+    { id: 2, visible: false },
+    { id: 3, visible: false },
+    { id: 4, visible: false }
+  ];
 
+  // Méthode pour basculer la visibilité d'un article
+  toggleContent(index: number) {
+    this.articles[index].visible = !this.articles[index].visible;
+  }
 }
+
